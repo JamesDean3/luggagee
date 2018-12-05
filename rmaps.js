fb74ab0443c772e09c52f13dff65d945d9f87830
JamesDean3 <13928304+JamesDean3@users.noreply.github.com>
2018-12-05 04:45:01 -0800

    Update rmaps.js

--- rmaps.js
+++ rmaps.js
@@ -1,7 +1,7 @@
 function rmapsload(){   
     var rMapsWindow;
     
-    document.getElementById("openRmapsWindow").onclick = function() {openRmapsURL()};
+    //document.getElementById("openRmapsWindow").onclick = function() {openRmapsURL()};
 	function openRmapsURL() {
   		rMapsWindow = window.open(
     		"https://jamesdean3.github.io/luggagee/",
@@ -9,12 +9,12 @@ function rmapsload(){
           )
 	}
     
-    document.getElementById("sendTorMaps").onclick = function() {sendMessageToRmaps()};
+    //document.getElementById("sendTorMaps").onclick = function() {sendMessageToRmaps()};
 	function sendMessageToRmaps() {
       rMapsWindow.postMessage("lets see", '*')
     }
     var el = document.createElement('div'),
         b = document.getElementsByTagName('body')[1];
-        el.innerHTML = "<p id='openRmapsWindow'>lets do this</p><br><p id='sendTorMaps'>then this</p>";
+        el.innerHTML = "<p id='openRmapsWindow' onclick='openRmapsURL();'>lets do this</p><br><p id='sendTorMaps' onclick='sendMessageToRmaps();'>then this</p>";
     b.appendChild(el);
 }
